590f4a433210ac6e38a60c9db9b04c192795e943
MAZ-ENG <3122085@stud.srh-campus-berlin.de>
2025-02-10 16:23:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 
+{
+  "name": "Python Development Environment",
+  "image": "mcr.microsoft.com/vscode/devcontainers/python:3.9",
+  "settings": {
+    "terminal.integrated.shell.linux": "/bin/bash"
+  },
+  "extensions": [
+    "ms-python.python",
+    "ms-python.vscode-pylance"
+  ],
+  "postCreateCommand": "pip install -r requirements.txt"
+}
